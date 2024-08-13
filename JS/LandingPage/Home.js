9878cd31907628aa3bc9526df29c4d454a7115c3
Abdo <abdonabil713@gmail.com>
2024-08-14 02:24:12 +0300

    adding arabic lang design

--- JS/LandingPage/Home.js
+++ JS/LandingPage/Home.js
@@ -1,95 +1,62 @@
 jQuery(document).ready(function() {
-    $('.menuBtn').click(function() {
-        $('header').toggleClass('collapsed')
-    })
-    
-    
-
-    
-
-    $(".ourFeatures").owlCarousel({
-        items:1 ,
-        loop:true,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            950 :{
-                items:3,
-            }
-        }
-    });
-    $(".payment-method-slider").owlCarousel({
-        items:1,
-        loop:true,
-        autoplay:true,
-        responsiveClass:true,
-        responsive:{
-            950 :{
-                items:3,
-            }
-        }
-    });
-    $("#productsSliders").owlCarousel({
-        items:1 ,
-        loop:true,
-        autoplay:true,
-        responsiveClass:true,
+    setTimeout(() => {
+        $('.menuBtn').click(function() {
+            $('header').toggleClass('collapsed')
+        })
         
-    });
-
+        
     
-
-
-    var bottom  = Math.floor($('.visitors-section').offset().top) + Math.floor($('.visitors-section').outerHeight(true))
-    var flag = true
-    $('section').each(function () {
-        if (Math.floor($(window).scrollTop()) >= Math.floor($(this).offset().top - 500) && Math.floor($(window).scrollTop()) <= ($(this).offset().top) + Math.floor($(this).outerHeight(true) )) {
-            $(this).find('.animationDiv').removeClass('start-50')
-    $(this).find(' .animationDiv').removeClass('end-50')
-    $(this).find('.animationDiv').removeClass('opacity-0')
+        
     
-    $(this).find(' .animationDiv').removeClass('start-50')
-    $(this).find(' .animationDiv').removeClass('end-50')
-    $(this).find(' .animationDiv').removeClass('opacity-0')
-    $(this).find(' .animationDiv').removeClass('top-60')
-        }     
-       })
-    if ( Math.floor($(window).scrollTop()) >= Math.floor($('.visitors-section').offset().top - 1000) && Math.floor($(window).scrollTop()) <=bottom ) {
-           
-       if(flag){
-           $('.counter-value').each(function () {
-               console.log($(this).prop('Counter',0));
-               $(this).prop('Counter',0).animate({
-                   Counter: $(this).text()
-               }, {
-                   duration: 4000,
-                   easing: 'swing',
-                   step: function (now) {
-                       $(this).text(Math.ceil(now));
-                   }
-               });
-           });
-       }
-       flag = false 
-   }
-     var $w = $(window).scroll(function () {
-       console.log(Math.floor($w.scrollTop()));
-       $('section').each(function () {
-        if (Math.floor($w.scrollTop()) >= Math.floor($(this).offset().top - 400) && Math.floor($(window).scrollTop()) <= ($(this).offset().top) + Math.floor($(this).outerHeight(true) )) {
-            $(this).find('.animationDiv').removeClass('start-50')
-    $(this).find(' .animationDiv').removeClass('end-50')
-    $(this).find('.animationDiv').removeClass('opacity-0')
+        $(".ourFeatures").owlCarousel({
+            items:1 ,
+            loop:true,
+            autoplay:true,
+            responsiveClass:true,
+            responsive:{
+                950 :{
+                    items:3,
+                }
+            }
+        });
+        $(".payment-method-slider").owlCarousel({
+            items:1,
+            loop:true,
+            autoplay:true,
+            responsiveClass:true,
+            responsive:{
+                950 :{
+                    items:3,
+                }
+            }
+        });
+        $("#productsSliders").owlCarousel({
+            items:1 ,
+            loop:true,
+            autoplay:true,
+            responsiveClass:true,
+            
+        });
     
-    $(this).find(' .animationDiv').removeClass('start-50')
-    $(this).find(' .animationDiv').removeClass('end-50')
-    $(this).find(' .animationDiv').removeClass('opacity-0')
-    $(this).find(' .animationDiv').removeClass('top-60')
-
-        }     
-       })
-       
-       if (Math.floor($w.scrollTop()) >= Math.floor($('.visitors-section').offset().top - 300) && Math.floor($(window).scrollTop()) <=bottom ) {
-           
+        
+    
+    
+        var bottom  = Math.floor($('.visitors-section').offset().top) + Math.floor($('.visitors-section').outerHeight(true))
+        var flag = true
+        $('section').each(function () {
+            if (Math.floor($(window).scrollTop()) >= Math.floor($(this).offset().top - 500) && Math.floor($(window).scrollTop()) <= ($(this).offset().top) + Math.floor($(this).outerHeight(true) )) {
+                $(this).find('.animationDiv').removeClass('start-50')
+        $(this).find(' .animationDiv').removeClass('end-50')
+        $(this).find('.animationDiv').removeClass('opacity-0')
+        
+        $(this).find(' .animationDiv').removeClass('start-50')
+        $(this).find(' .animationDiv').removeClass('end-50')
+        $(this).find(' .animationDiv').removeClass('opacity-0')
+        $(this).find(' .animationDiv').removeClass('top-60')
+            }     
+           })
+        if ( Math.floor($(window).scrollTop()) >= Math.floor($('.visitors-section').offset().top - 1000) && Math.floor($(window).scrollTop()) <=bottom ) {
+               
            if(flag){
                $('.counter-value').each(function () {
                    console.log($(this).prop('Counter',0));
@@ -106,6 +73,41 @@ jQuery(document).ready(function() {
            }
            flag = false 
        }
-   });
-
+         var $w = $(window).scroll(function () {
+           console.log(Math.floor($w.scrollTop()));
+           $('section').each(function () {
+            if (Math.floor($w.scrollTop()) >= Math.floor($(this).offset().top - 400) && Math.floor($(window).scrollTop()) <= ($(this).offset().top) + Math.floor($(this).outerHeight(true) )) {
+                $(this).find('.animationDiv').removeClass('start-50')
+        $(this).find(' .animationDiv').removeClass('end-50')
+        $(this).find('.animationDiv').removeClass('opacity-0')
+        
+        $(this).find(' .animationDiv').removeClass('start-50')
+        $(this).find(' .animationDiv').removeClass('end-50')
+        $(this).find(' .animationDiv').removeClass('opacity-0')
+        $(this).find(' .animationDiv').removeClass('top-60')
+    
+            }     
+           })
+           
+           if (Math.floor($w.scrollTop()) >= Math.floor($('.visitors-section').offset().top - 300) && Math.floor($(window).scrollTop()) <=bottom ) {
+               
+               if(flag){
+                   $('.counter-value').each(function () {
+                       console.log($(this).prop('Counter',0));
+                       $(this).prop('Counter',0).animate({
+                           Counter: $(this).text()
+                       }, {
+                           duration: 4000,
+                           easing: 'swing',
+                           step: function (now) {
+                               $(this).text(Math.ceil(now));
+                           }
+                       });
+                   });
+               }
+               flag = false 
+           }
+       });
+     
+    }, 50);
 })
--- JS/LandingPage/shared.js
+++ JS/LandingPage/shared.js
@@ -1,7 +1,14 @@
 jQuery(document).ready(function() {
+    
+    if($('body').attr('dir')=='rtl'){
+        $('link[href="../../CSS/shared/bootstrap.min.css"]').attr('href','../../CSS/shared/bootstrapArabic.min.css');   
+    }
     $(window).scroll(function(){ 
+        
 
         var scrollPos = $(window).scrollTop();
+
+        
     
         if( ( scrollPos >100 )  ) {
             $('header').removeClass('py-3')
