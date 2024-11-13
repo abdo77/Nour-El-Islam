b5c7838a53225610def02bf2eb956376af0cef14
Abdo <abdonabil713@gmail.com>
2024-11-13 08:11:18 +0200

    language of lamding page

--- JS/LandingPage/Home.js
+++ JS/LandingPage/Home.js
@@ -1,8 +1,6 @@
 jQuery(document).ready(function() {
     setTimeout(() => {
-        $('.menuBtn').click(function() {
-            $('header').toggleClass('collapsed')
-        })
+        
         
         
     
--- JS/LandingPage/shared.js
+++ JS/LandingPage/shared.js
@@ -38,4 +38,31 @@ jQuery(document).ready(function() {
         
     
     });
+
+
+    $('.language a').click(function(){
+        if($(this).prop('id')=='Arabic'){
+            $('link[href="../../CSS/shared/bootstrap.min.css"]').attr('href','../../CSS/shared/bootstrapArabic.min.css')
+            $('body').attr('dir','rtl')
+            localStorage.setItem('language',$(this).prop('id'))
+        }
+        else{
+            $('link[href="../../CSS/shared/bootstrapArabic.min.css"]').attr('href','../../CSS/shared/bootstrap.min.css')
+            $('body').attr('dir','ltr')
+            localStorage.setItem('language',$(this).prop('id'))
+        }
+    })
+
+    language = localStorage.getItem('language')
+
+    console.log(language);
+    
+    if(language=='Arabic'){
+        $('link[href="../../CSS/shared/bootstrap.min.css"]').attr('href','../../CSS/shared/bootstrapArabic.min.css')
+        $('body').attr('dir','rtl')
+    }
+    else{
+        $('link[href="../../CSS/shared/bootstrapArabic.min.css"]').attr('href','../../CSS/shared/bootstrap.min.css')
+        $('body').attr('dir','ltr')
+    }
 })
